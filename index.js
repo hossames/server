939b59742172195430ed1e1e64faaa174c685a54
hossames <100873739+hossames@users.noreply.github.com>
2024-07-29 19:25:07 +0300

    done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
@@ -6,7 +8,13 @@ const cors = require('cors');
 
 const multer = require('multer');
 
-const cloudinary = require('./cloudinary.js');
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME , 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_PASSWORD
+});
 
 const storage = multer.diskStorage({
     filename: (req, file, cb) => {
@@ -16,7 +24,58 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-const modules = require('./mongo');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_KEY).then(()=>{
+    console.log('love');
+}).catch(()=>{
+    console.log('no love');
+});
+
+const schema = mongoose.Schema({
+    catagory :{
+        type : String,
+        required : true
+    }
+})
+
+const catagory = mongoose.Schema({
+    catagory :{
+        type : String,
+        required : true
+    }
+})
+const product = mongoose.Schema({
+    catagory :{
+        type : String,
+        required : true
+    },
+    image :{
+        type : String,
+        required : true
+    },
+    name :{
+        type : String,
+        required : true
+    },
+    id :{
+        type : String,
+        required : true
+    },
+    discription :{
+        type : String,
+        required : true
+    },
+    specs :{
+        type : Array,
+        required : true
+    }
+
+})
+
+const collections = new mongoose.model('collections', catagory);
+
+const products = new mongoose.model('products',product);
+
 
 app.use(cors());
 
@@ -24,7 +83,7 @@ app.use(express.json({limit:'50mb'}));
 
 app.use(express.urlencoded({ extended: false }));
 
-app.post('/api/upload',upload.single(),async (req, res) => {
+app.post('/upload',upload.single(),async (req, res) => {
     try {
         const result = await cloudinary.uploader.upload_large(req.body.image,{
             folder: '',
@@ -36,39 +95,39 @@ app.post('/api/upload',upload.single(),async (req, res) => {
     }
 })
 
-app.get('/api/upload', async (req, res) => {
+app.get('/upload', async (req, res) => {
     res.send('shit');
 })
 
-app.post('/api', async (req, res) => {
-    const user = await new modules.collections(req.body);
+app.post('/', async (req, res) => {
+    const user = await new collections(req.body);
     new_user = await user.save();
     res.send(new_user);
 });
 
-app.get('/api', async (req, res) => {
-    const users = await modules.collections.find();
+app.get('/', async (req, res) => {
+    const users = await collections.find();
     await res.status(200).json(users);
 });
 
-app.delete('/api', async (req, res) => {
-    const user = await modules.collections.findByIdAndDelete(req.body.id);
+app.delete('/', async (req, res) => {
+    const user = await collections.findByIdAndDelete(req.body.id);
     res.send(user);
 })
 
-app.get('/api/product', async (req, res) => {
-    const users = await modules.products.find();
+app.get('/product', async (req, res) => {
+    const users = await products.find();
     await res.status(200).json(users);
 });
 
-app.post('/api/product', async (req, res) => {
-    const user = await new modules.products(req.body);
+app.post('/product', async (req, res) => {
+    const user = await new products(req.body);
     new_user = await user.save();
     res.send(new_user);
 });
 
-app.delete('/api/product', async (req, res) => {
-    const user = await modules.products.findByIdAndDelete(req.body.id);
+app.delete('/product', async (req, res) => {
+    const user = await products.findByIdAndDelete(req.body.id);
     res.send(user);
 })
 
