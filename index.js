3ebe52c95bab2d342cd67268b247d43a96bf845b
hossames <100873739+hossames@users.noreply.github.com>
2024-07-29 18:30:31 +0300

    new folders and files

--- index.js
+++ index.js
@@ -72,6 +72,6 @@ app.delete('/product', async (req, res) => {
     res.send(user);
 })
 
-app.listen(3000,()=>{
+app.listen(3000||process.env.PORT,()=>{
     console.log('Server started on port 3000');
 });
